cafaf7cc60636875ac358d0228c955e29f4d3324
Zaid Annas <m.zaid.annas@gmail.com>
2025-06-04 13:02:22 +0500

    Updated spring boot to version 3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.springframework.boot.gradle.tasks.aot.ProcessAot
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.6"
-	id("io.freefair.lombok") version "8.7.1"
-	id("org.graalvm.buildtools.native") version "0.10.2"
+	id("io.freefair.lombok") version "8.13.1"
+	id("org.graalvm.buildtools.native") version "0.10.6"
 }
 
 group = "com.assessment"
@@ -21,14 +21,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	// version >= 42.2.x is required to support newer postgresql password encryption SCRAM-SHA-256
-	implementation("org.postgresql:postgresql:42.7.3")
-	implementation("org.springframework.boot:spring-boot-starter-validation:3.3.2")
-	implementation("org.modelmapper:modelmapper:3.2.1")
-	implementation("jakarta.validation:jakarta.validation-api:3.1.0")
+	implementation("org.postgresql:postgresql:42.7.6")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("jakarta.validation:jakarta.validation-api:3.1.1")
 	implementation("jakarta.transaction:jakarta.transaction-api:2.0.1")
 	implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-	testImplementation("junit:junit:4.13.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/test/java/com/assessment/payment/service/PaymentServiceTest.java
+++ src/test/java/com/assessment/payment/service/PaymentServiceTest.java
@@ -21,8 +21,8 @@ import org.springframework.http.ResponseEntity;
 import java.util.Collections;
 import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.anyInt;
 import static org.mockito.Mockito.doReturn;
